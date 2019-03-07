892e47316bc5c5761d25866c6de7c419cdf191c1
Diord <Diord@DESKTOP-6B09NPQ>
2019-03-07 15:44:17 +0300

    Add project files.

---
+++ Distribution/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Distribution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.uniformButton = new System.Windows.Forms.Button();
+            this.erlangButton = new System.Windows.Forms.Button();
+            this.bBox = new System.Windows.Forms.TextBox();
+            this.aBox = new System.Windows.Forms.TextBox();
+            this.scaleBox = new System.Windows.Forms.TextBox();
+            this.shapeBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uniformButton
+            // 
+            this.uniformButton.Location = new System.Drawing.Point(384, 511);
+            this.uniformButton.Name = "uniformButton";
+            this.uniformButton.Size = new System.Drawing.Size(209, 73);
+            this.uniformButton.TabIndex = 0;
+            this.uniformButton.Text = "Uniform";
+            this.uniformButton.UseVisualStyleBackColor = true;
+            this.uniformButton.Click += new System.EventHandler(this.uniformButton_Click);
+            // 
+            // erlangButton
+            // 
+            this.erlangButton.Location = new System.Drawing.Point(599, 511);
+            this.erlangButton.Name = "erlangButton";
+            this.erlangButton.Size = new System.Drawing.Size(209, 73);
+            this.erlangButton.TabIndex = 1;
+            this.erlangButton.Text = "Erlang";
+            this.erlangButton.UseVisualStyleBackColor = true;
+            this.erlangButton.Click += new System.EventHandler(this.erlangButton_Click);
+            // 
+            // bBox
+            // 
+            this.bBox.Location = new System.Drawing.Point(408, 479);
+            this.bBox.Name = "bBox";
+            this.bBox.Size = new System.Drawing.Size(185, 26);
+            this.bBox.TabIndex = 4;
+            // 
+            // aBox
+            // 
+            this.aBox.Location = new System.Drawing.Point(408, 445);
+            this.aBox.Name = "aBox";
+            this.aBox.Size = new System.Drawing.Size(185, 26);
+            this.aBox.TabIndex = 5;
+            // 
+            // scaleBox
+            // 
+            this.scaleBox.Location = new System.Drawing.Point(658, 479);
+            this.scaleBox.Name = "scaleBox";
+            this.scaleBox.Size = new System.Drawing.Size(150, 26);
+            this.scaleBox.TabIndex = 6;
+            // 
+            // shapeBox
+            // 
+            this.shapeBox.Location = new System.Drawing.Point(658, 447);
+            this.shapeBox.Name = "shapeBox";
+            this.shapeBox.Size = new System.Drawing.Size(150, 26);
+            this.shapeBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(380, 448);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(380, 479);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(602, 482);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "scale:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(595, 451);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "shape:";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(593, 439);
+            this.chart1.TabIndex = 16;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(599, 0);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(593, 439);
+            this.chart2.TabIndex = 17;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1195, 596);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.aBox);
+            this.Controls.Add(this.bBox);
+            this.Controls.Add(this.scaleBox);
+            this.Controls.Add(this.shapeBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.erlangButton);
+            this.Controls.Add(this.uniformButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uniformButton;
+        private System.Windows.Forms.Button erlangButton;
+        private System.Windows.Forms.TextBox bBox;
+        private System.Windows.Forms.TextBox aBox;
+        private System.Windows.Forms.TextBox scaleBox;
+        private System.Windows.Forms.TextBox shapeBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ Distribution/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using CenterSpace.NMath.Stats;
+using CenterSpace.NMath.Core;
+using CenterSpace.NMath.Charting.Microsoft;
+
+namespace Distribution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void uniformButton_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            double a,b;
+            if (double.TryParse(aBox.Text, out a) && double.TryParse(bBox.Text, out b))
+            {
+                if (a != b)
+                {
+                    UniformDistribution uniform = new UniformDistribution(a, b);
+                    //PDF
+                    for (double x = 0.1; x < b + (Math.Abs(a-b)/2); x = x + 0.1)
+                    {
+                        chart1.Series[0].Points.AddXY(x, uniform.PDF(x));
+                    }
+                    //CDF
+                    for (double x = 0.1; x < b + (Math.Abs(a-b)/2) ; x = x + 0.1)
+                    {
+                        chart2.Series[0].Points.AddXY(x, uniform.CDF(x));
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("There must be a gap between a and b");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wrong input! Try again.");
+            }
+        }
+
+        private void erlangButton_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            int k;
+            double u;
+            if(int.TryParse(shapeBox.Text, out k) && double.TryParse(scaleBox.Text, out u))
+            {
+                GammaDistribution gamma = new GammaDistribution(k, u);
+                //PDF
+                for(double x = 0.1; x<10*k*u; x=x+0.1)
+                {
+                    chart1.Series[0].Points.AddXY(x,gamma.PDF(x));
+                }
+                //CDF
+                for (double x = 0.1; x < 20*u; x = x + 0.1)
+                {
+                    chart2.Series[0].Points.AddXY(x, gamma.CDF(x));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wrong input! Try again.");
+            }  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            chart2.Series[0].ChartType = SeriesChartType.Line;
+            chart1.Legends.Clear();
+            chart2.Legends.Clear();
+            chart1.Titles.Add("Probability density function");
+            chart2.Titles.Add("Cumulative distribution function");
+        }
+    }
+}
